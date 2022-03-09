5e9d0d089ceb46d78d2c33a5804f411d1a5dfc24
thabie-dev <taniatdube1@gmail.com>
2022-03-09 14:29:40 +0200

    actual work

--- index.js
+++ index.js
@@ -1,86 +1,27 @@
+const dec = document.querySelector('.dec');
+const asc = document.querySelector('.asc');
+const show = document.querySelector('.show');
+
+var outPutLbl = document.querySelector('.outPutLbl');
+
 const fruits = ["Grapes 🍇", "Melon 🍈", "Watermelon 🍉", "Tangerine 🍊", "Lemon 🍋", "Banana 🍌", "Pineapple 🍍", "Mango 🥭", "Red Apple 🍎 "];
 
-
- function makeList(){
-     let listData =[
-         'Grapes 🍇', 
-         'Melon 🍈',
-         'Watermelon 🍉',
-         'Tangerine 🍊', 
-         'Lemon 🍋', 
-         'Banana 🍌', 
-         'Pineapple 🍍',
-         'Mango 🥭', 
-         'Red Apple 🍎'
-
-     ],
-
-  
-      // Make a container element for the list
-    listContainer = document.createElement('item'),
-    // Make the list
-    listElement = document.createElement('fruits'),
-    // Set up a loop that goes through the items in listItems one at a time
-    numberOfListItems = listData.length,
-    listItem,
-    i;
-
-    // Add it to the page
-    document.getElementsByTagName('item')[0].appendChild(listContainer);
-    listContainer.appendChild(listElement);
-
-    for (i = 0; i < numberOfListItems; ++i) {
-        // create an item for each one
-        listItem = document.createElement('li');
-
-        // Add the item text
-        listItem.innerHTML = listData[i];
-
-        // Add listItem to the listElement
-        listElement.appendChild(listItem);
-    }
-
-    
-    //add new fruits
-    const list = document.querySelector('ul');
-    const input = document.querySelector('input');
-    const button= document.querySelector('button');
-    button.onclick = function() {
-        let myItem =input.value;
-        input.value ='';
-
-    const listItem =document.createElement('li');
-    const listText =document.createElement('spa');
-    const listBtn =document.createElement('button');
-    
-    listItem.appendChild(listText);
-    listText.textContent = myItem;
-    listItem.appendChild(listBtn);
-    listBtn.textContent = 'delete';
-    list.appendChild(listItem);
-
-    listBtn.onclick = function(e){
-        list.removeChild(listItem);
-    }
-    input.focus();
-    }   
-}
-
-//ascending and descending 
-    
-function myFunction1(){
-  value.sort();
-    document.getElementById("values").innerHTML = value;
-}
-
-function myFunction2(){
-    value.sort();
-    value.reverse();
-    document.getElementById("values").innerHTML = value;
-}
-
-
-// Usage
-makeList();
-
-
+//show fruits 
+show.addEventListener('click', function () {
+    alert("friuts added");
+    outPutLbl.innerHTML = fruits;
+});
+ //ascending 
+asc.addEventListener('click', function () {
+    alert("friuts sorted in ascending");
+    var ascfruits = fruits.sort();
+    outPutLbl.innerHTML = ascfruits;
+});
+//descending 
+dec.addEventListener('click', function () {
+    alert("friuts decending");
+    var ascfruits = fruits.sort();
+    var decfruits = ascfruits.reverse();
+    outPutLbl.innerHTML = decfruits;
+});
+ 
